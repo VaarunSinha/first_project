506fc5ad85fd779ef1705e419f150d370b509090
Vaarun Sinha <85062344+VaarunSinha@users.noreply.github.com>
2024-08-15 13:53:40 +0530

    Models created.

--- first_project/settings.py
+++ first_project/settings.py
@@ -120,5 +120,8 @@ STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+import os
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    banner = models.ImageField(upload_to="news/banner")
+    content = models.TextField(max_length=2000)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
