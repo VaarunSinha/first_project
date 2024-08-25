2159e900e6f722bca72e48dd297aadc3f1dff05d
Vaarun Sinha <85062344+VaarunSinha@users.noreply.github.com>
2024-08-25 17:19:39 +0530

    Task models Created

--- first_project/settings.py
+++ first_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "news",
     "authentication",
     "author",
+    "tasks",
 ]
 
 MIDDLEWARE = [
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from authentication.models import Author
+
 
 # Create your models here.
+class Task(models.Model):
+    task = models.CharField(max_length=300)
+    is_complete = models.BooleanField(default=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
