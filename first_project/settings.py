81f9986f42dca60b068079d2196bec1adf49656d
Vaarun Sinha <85062344+VaarunSinha@users.noreply.github.com>
2024-09-24 16:16:58 +0530

    Celery Added

--- first_project/settings.py
+++ first_project/settings.py
@@ -145,3 +145,6 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
